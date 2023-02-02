fbc33950ff2942156ffbb1c0a4d6481cbe4f9809
Ivan Novakov <inovakov.bg@gmail.com>
2023-02-02 19:41:50 +0200

    Add
    add lessons

--- src/App.js
+++ src/App.js
@@ -2,39 +2,145 @@
 import React, { Component } from "react";
 import './App.css';
 
-//BUTTON ICREMENT
+//Потребителски данни и други евенти 1:05 - 1:22
 
 class App extends Component {
 
   constructor(props) {
     super(props);
-    this.state = { counter: 0 };
+    this.state = {
+      name: "",
+      email: "",
+      gender: "",
+    };
   }
 
-  increment = () => {
-    this.setState((prevState) => { return { counter: ++prevState.counter }; });
+  handleInput = (event) => {
+    this.setState({ [event.target.id]: event.target.value });
   }
 
-  reset=() => {
-  this.setState({ counter: 0 });
+  handleSubmit = (event) => {
+    event.preventDefault();
+
+    console.log(this.state);
+
   }
 
-  render(){
-    return(
-        <div className="App">
-          <h2>Сегашният counter e {this.state.counter}</h2>
-          <button onClick={this.increment}>Increment</button>
-          <button onClick={this.reset}>Reset </button>
-        
+  render() {
+    return (
+      <div className="App" style={{ padiing: 25 }}>
+        <form onSubmit={this.handleSubmit} >
+          <input id='name' value={this.state.name} onChange={this.handleInput} type="text" placeholder="име" autoComplete="off" />
+          <input id='email' value={this.state.email} onChange={this.handleInput} type="email" placeholder="e-mail" autoComplete="off" />
+          <br />
+          <select id="gender" value={this.state.gender} onChange={this.handleInput}>
+
+
+            <option value=""></option>
+            <option value="мъж">Мъж</option>
+            <option value="жена">Жена</option>
+
+          </select>
+          <br /><br />
+          <input type="submit" value="Потвърди" />
+
+        </form>
+
+        <hr />
+
+        <div style={{ textAlign: "left" }}>
+          <h2>Име: {this.state.name}</h2>
+          <h2>Е-mail: {this.state.email}</h2>
+          <h2>Пол: {this.state.gender}</h2>
+
         </div>
+      </div>
 
-    );
-
+    )
   }
-
-
 
 }
+
+
+//LIFE CYCLE 55-1:05
+
+// class App extends Component {
+
+//   constructor(props) {
+//     super(props);
+//     this.state = { counter: 0 };
+//   }
+
+//   componentDidMount() {
+//     this.timer=setInterval(this.increment,50);
+//   }
+
+//   componentDidUpdate(prevProops,prevState){
+//     console.log("Ready");
+//     if( prevState.counter===50 ) {this.setState({counter:0}) ;}
+
+//   }
+
+//   componentWillUnmount(){
+//     clearInterval(this.timer);
+//   }
+
+
+
+//   increment = () => {
+//     this.setState((prevState) => { return { counter: ++prevState.counter }; });
+//   }
+
+//   render() {
+//     return (
+//       <div className="App">
+//         <h2>Сегашният counter e {this.state.counter}</h2>
+//       </div>
+
+//     )
+//   }
+// }
+
+
+
+//BUTTON ICREMENT 43-55
+
+// class App extends Component {
+
+//   constructor(props) {
+//     super(props);
+//     this.state = { counter: 0 };
+//   }
+
+//   increment = () => {
+//     this.setState((prevState) => { return { counter: ++prevState.counter }; });
+//   }
+
+//   decrement =() => {
+//     this.setState((aprev) => { return {counter: --aprev.counter };} );
+//   }
+
+//   reset=() => {
+//   this.setState({ counter: 0 });
+//   }
+
+//   render(){
+//     return(
+//         <div className="App">
+//           <h2>Сегашният counter e {this.state.counter}</h2>
+//           <button onClick={this.decrement}>Decrement</button>
+//           <button onClick={this.increment}>Increment</button>
+//           <button onClick={this.reset}>Reset </button>
+
+//         </div>
+
+//     );
+
+//   }
+
+
+
+// }
 
 
 
